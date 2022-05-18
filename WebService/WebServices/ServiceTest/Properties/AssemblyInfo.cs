67318c5647c189a3adda03e8e177da1689a15378
neontodo <negreaandrei90@gmail.com>
2022-05-18 19:21:25 +0300

    few tests

---
+++ WebService/WebServices/ServiceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ServiceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServiceTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d38d4a6-9df3-4ef2-8ea8-fedc66fe2226")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebService/WebServices/ServiceTest/UnitAuthenticationTest.cs
@@ -0,0 +1,104 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ServiceTest
+{
+    [TestFixture]
+    public class UnitAuthenticationTest
+    {
+        ServiceReference1.WebAuthenticationSoapClient service = new ServiceReference1.WebAuthenticationSoapClient();
+
+        //Tests with focus on FindAccounnt possible cases
+        [Test]
+        public void FindAccount_WasFound()
+        {
+            int id;
+            string username = "admin";
+            string password = "admin";
+
+            id = service.FindAccount(username, password);
+
+            Assert.AreEqual(id, 1);
+        }
+
+        [Test]
+        public void FindAccount_WasNotFound()
+        {
+            int id;
+            string username = "maricica";
+            string password = "parola";
+
+            id = service.FindAccount(username, password);
+
+            Assert.AreEqual(id, -1);
+        }
+
+
+        //tests with focus on isEmployee possible cases
+        [Test]
+        public void isEmployee_Positive()
+        {
+            int id = 1; //admin
+            bool result;
+
+            result = service.IsEmployee(id);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void isEmployee_Negative()
+        {
+            int id = 48;  //normal account
+            bool result;
+
+            result = service.IsEmployee(id);
+
+            Assert.That(result, Is.False);
+        }
+
+        //tests with focus on isClient possible cases
+        [Test]
+        public void isClient_Positive()
+        {
+            int id = 48; //normal account
+            bool result;
+
+            result = service.IsClient(id);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void isClient_Negative()
+        {
+            int id = 1; //admin
+            bool result;
+
+            result = service.IsClient(id);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void CreateAccount_Succesful()
+        {
+            //username and password for account
+            string username = "test";
+            string password = "test";
+            List<string> details = new List<string>();
+            details.Add(username);
+            details.Add(password);
+
+            bool result;
+
+            result = service.CreateAccount(details);
+
+            Assert.That(result, Is.True);
+
+        }
+    }
+}
